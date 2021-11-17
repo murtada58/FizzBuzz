537bfcf6a9ac44711ffa8947e1883f2cfd757799
murtada58 <murtada58@hotmail.com>
2021-11-17 13:24:47 +0000

    implemented simple fizzbuzz with if statments

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,18 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int startCount = 1;
+            int endCount = 100;
+            int fizz = 3;
+            int buzz = 5;
+            
+            for (int i = startCount; i <= endCount; i++)
+            {
+                if (i % (fizz * buzz) == 0) { Console.WriteLine("FizzBuzz"); }
+                else if (i % fizz == 0) { Console.WriteLine("Fizz"); }
+                else if (i % buzz == 0) { Console.WriteLine("Buzz"); }
+                else { Console.WriteLine(i); }
+            }
         }
     }
 }
